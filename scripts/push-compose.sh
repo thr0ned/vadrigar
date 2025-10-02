207f5506052564fa405ccc1f74631e3ab3dde1a9
thr0ned <nofragsthrone@gmail.com>
2025-10-02 04:49:15 +0200

    kek2

--- scripts/push-compose.sh
+++ scripts/push-compose.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-scp -r ${BASE_DIR}/docker vadrigar:/home/vadrigar
-
+rsync -avh server/ sysadmin:/home/vadrigar/server/ \
+    --chown=vadrigar:vadrigar
